67ecee47c2a806d7dd9c5f0b25ca869d66335ccd
kleddy17 <kleddy17@gmail.com>
2020-12-30 17:12:10 -0500

    added skel form

--- src/components/Network.js
+++ src/components/Network.js
@@ -1,6 +1,7 @@
 import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import authHeader from '../utilities/authHeader.utilities'
+import { Form, Button } from 'react-bootstrap'
 
 const Network = () => {
     
@@ -11,31 +12,70 @@ const Network = () => {
         getNetwork()
     },[])
 
+    // Getting the data from our API (database/collection)
     const getNetwork = () =>{
         const network = axios.get("http://localhost:8080/findNetwork", {headers: authHeader()})
         .then(network =>
             setNetworkData(network.data))
          
     }
-  console.log(networkData)
 
-  console.log(setNetworkData)
     return (
-        // {currentUser.roles &&
-        //     currentUser.roles.map((role, index)=> <li key={index}> {role} </li>)
-        // }
-        
-       
-         <div>
-              <h1 style = {{textAlign: "center"}}>  Network: </h1>
-            <ul> 
-                {networkData.map((network, index) =>(
-                    <li 
-                        key={index}> <b> Name:</b> {network.name} <b> Company:</b>  {network.company} <b> Phone:</b> {network.phone} <b> Email:</b> {network.email} <b> Notes:</b> {network.notes} 
-                    </li>
-                ))}
-            </ul>
+
+        <div>
+            <div className = "showsavednetwork">
+              <h1 style = {{textAlign: "center"}}>Network Roledex</h1>
+                <ul> 
+                    {networkData.map((network, index) =>(
+                        <li 
+                            key={index}> 
+                            <b> Name:</b> {network.name} <br></br>
+                            <b> Company:</b>{network.company}<br></br>
+                            <b> Phone:</b> {network.phone}<br></br>
+                            <b> Email:</b> {network.email}<br></br>
+                            <b> Notes:</b> {network.notes} <br></br>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+            <br></br>
+            <br></br>
+                <h3>Add a new contact:</h3>
+                <Form>
+                    <Form.Group controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="name" placeholder="Name" />
+                        <Form.Text className="text-muted">
+                            
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCompany">
+                        <Form.Label>Company</Form.Label>
+                        <Form.Control type="company" placeholder="company" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPhone">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control type="phone" placeholder="phone number" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="email" />
+                    </Form.Group>
+                       
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                </Form>
+            </div>
         </div>
+
+                    
+
     )
 }
 
