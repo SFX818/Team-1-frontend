655ee736d066c9c64249a1465b6ee106da7c93a3
Ronikhudanyan <Ronikhudanyan@gmail.com>
2020-12-29 21:33:06 -0800

    added pagination to the rob results page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import FetchJobs from '../utilities/fetchJobs.utilities'
 import {Container} from 'react-bootstrap'
 import Job from './Job'
+import JobsPagination from './JobsPagination'
 
 const Landing = () => {
     // passing in params and page numbers to set the state to original status, empty object meaning no results bc no search has been made yet and default page 1 
@@ -10,9 +11,11 @@ const Landing = () => {
     const {jobs, loading, error } = FetchJobs(params, page)
     return (
         <Container>
+            <h1 className= 'mb-4'>.find(career)</h1>
+            <h2 className='mb-4'>returned(results):</h2>
+            <JobsPagination page={page} setPage={setPage}/>
             {loading && <h1>Loading...</h1>}
             {error && <h1> Error. Try Refreshing </h1>}
-            {<h1>{jobs.length}</h1>}
             {/* go through all the jobs in our search query and render the HTML to show a job card */}
             {jobs.map(job => {
                 // Job component needs key because its looping, then it will pass down the job to the key which will in tern pass to empty object
