755f1f55246240d7745d56dec27c14e5132e354d
kleddy17 <kleddy17@gmail.com>
2020-12-30 13:42:46 -0500

    fetching network from our api and display one attribute

--- src/components/Network.js
+++ src/components/Network.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+import authHeader from '../utilities/authHeader.utilities'
 
 const Network = () => {
+    
+    // Setting state to hold the saved networks
+    const [networkData, setNetworkData] = useState([])
+
+    useEffect (()=>{
+        getNetwork()
+    },[])
+
+    const getNetwork = () =>{
+        const network = axios.get("http://localhost:8080/findNetwork", {headers: authHeader()})
+        .then(network =>
+            setNetworkData(network.data))
+         
+    }
+  console.log(networkData)
+
     return (
-        <div>
+       
+         <div>
+                    <h2>Name:{networkData[0].name}</h2>
+                    
             
         </div>
     )
