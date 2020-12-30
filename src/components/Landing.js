0a7175956890180079d24122904b79361bc3f6ac
Ronikhudanyan <Ronikhudanyan@gmail.com>
2020-12-29 22:06:57 -0800

    added the search functionality to the jobs search page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,16 +3,27 @@ import FetchJobs from '../utilities/fetchJobs.utilities'
 import {Container} from 'react-bootstrap'
 import Job from './Job'
 import JobsPagination from './JobsPagination'
+import SearchForm from './SearchForm'
 
 const Landing = () => {
     // passing in params and page numbers to set the state to original status, empty object meaning no results bc no search has been made yet and default page 1 
     const [params, setParams] = useState({})
     const [page, setPage] = useState(1)
     const { jobs, loading, error, hasNextPage } = FetchJobs(params, page)
+
+    function handleParamChange(e) {
+        const param = e.target.name
+        const value = e.target.value
+        setPage(1)
+        setParams(prevParams => {
+          return { ...prevParams, [param]: value }
+        })
+    }
     return (
         <Container>
             <h1 className= 'mb-4'>.find(career)</h1>
             <h2 className='mb-4'>returned(results):</h2>
+            <SearchForm params={params} onParamChange={handleParamChange} />
             <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
                     {loading && <h1>Loading...</h1>}
                     {error && <h1>Error. Try Refreshing.</h1>}
