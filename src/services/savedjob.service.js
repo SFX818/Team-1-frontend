7076dfd95e0972adacfd489de975937eb81145b2
Ronikhudanyan <Ronikhudanyan@gmail.com>
2020-12-30 19:28:01 -0800

    cleaned up search code

--- src/services/savedjob.service.js
+++ src/services/savedjob.service.js
@@ -2,45 +2,16 @@ import axios from 'axios';
 
 import { setItem, getItem, removeItem } from '../utilities/localStorage.utilities'
 
-import authHeader from '../utilities/authHeader.utilities'
 
-//IMPORTANT NOTE: FOR ALL THE ROUTES THAT USE AUTHJWTS.VERIFYWEBTOKEN YOU HAVE TO PASS IN HEADERS THAT IS RUNNING THE AUTHHEADER FUNCTION WHICH RETURNS THE ACCESS TOKEN
-//HEADERS MUST BE PASSED IN WITH AN S, PLURAL, BECAUSE THE BACK END IS EXPECTING REQ.HEADERS
-// export const getJobs = () => {
-//     return axios
-//     .get("http://localhost:8080/profile/savedJobs", {headers: authHeader()})
-//     .then((jobsResponse) => {
-//         console.log('jobsResponse', jobsResponse);
-        
-//         if(jobsResponse.data){
-//         setItem("jobslist", jobsResponse)
-//         }
-//         return(jobsResponse)
-//     })
-//     .catch(err => {
-//         console.log('error on getJobs function in service', err)
-//     })
-// }
+export const saveAJob = (id, location, company, jobTitle) => {
+    return axios
+    .post('http://localhost:8080/newsavedjob', {id, location, company, jobTitle})
+    // .post('http://localhost:8080/newsavedjob', {headers: authHeader()})
+    .then(response => {
+        console.log('post response', response);
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
 
-// export const getSavedJobs = () => {
-//     return getItem("savedJobs")
-// }
-
-// export const getJobs = () => {
-//     return axios
-//     .get("http://localhost:8080/profile/savedJobs", {headers: authHeader()})
-//     .then((jobsResponse) => {
-//         console.log('jobsResponse', jobsResponse);
-//         return(jobsResponse)
-//     })
-//     .catch(err => {
-//         console.log('error on getJobs function in service', err)
-//     })
-// }
-
-// export const jobGrabber = async () => {
-//     const jobsResponse = await axios
-//     .get("http://localhost:8080/profile/savedJobs", {headers: authHeader()})
-//     setAllJobs(jobsResponse.data)
-//     console.log(jobsR)
-// }
