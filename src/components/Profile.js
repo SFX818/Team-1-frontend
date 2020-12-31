811c6db22458d023aa7808c5b4aff0873ca9b152
Paola Tramontin <paolatramontin@Paolas-MacBook-Pro.local>
2020-12-30 21:17:07 -0500

    added 2 pie charts and cards to display job names on profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { getCurrentUser } from "../services/auth.service";
 import axios from "axios";
 import authHeader from "../utilities/authHeader.utilities";
+import { Link } from "react-router-dom";
+import { PieChart } from "react-minimal-pie-chart";
+import "../css/App.css";
+import { Card, Button } from "react-bootstrap";
 
 //backend function import
 import { getJobs } from "../services/savedjob.service";
@@ -26,12 +30,16 @@ const Profile = () => {
   const listJobs = () => {
     if (allJobs.length > 0) {
       return allJobs[0].allJobs.map((job, index) => (
-        <ul>
-          <li key={index}>
-            {" "}
-            <b> Company:</b> {job.company} <b> Job Title:</b> {job.jobTitle}
-          </li>
-        </ul>
+        <div id="card">
+          <Card body>
+            <ul>
+              <li key={index}>
+                {" "}
+                <b> Company:</b> {job.company} <b> Job Title:</b> {job.jobTitle}
+              </li>
+            </ul>
+          </Card>
+        </div>
       ));
     }
   };
@@ -41,14 +49,78 @@ const Profile = () => {
       <div className="container">
         <header className="jumbotron">
           <h3>
-            <strong> {currentUser.username} </strong>
+            <strong> Welcome {currentUser.username} </strong>
           </h3>
         </header>
 
         <h2>{allJobs.appliedToJobs && allJobs.appliedToJobs[0].company}</h2>
 
+        {/* <h3>All Jobs: {allJobs[0].allJobs.length} </h3>
+        <h3>Applied: {allJobs[0].appliedToJobs.length} </h3>
+        <h3>Heard Back: {allJobs[0].heardBackJobs.length} </h3>
+        <h3>Denied: {allJobs[0].deniedFromJobs.length} </h3> */}
+
+        <h2> Saved Jobs: </h2>
         {listJobs()}
-        {allJobs[0].heardBackJobs.length}
+
+        <div id="bigPie">
+          <div id="pie">
+            <h2 id="applied">
+              {" "}
+              Applied to: {allJobs[0].appliedToJobs.length}{" "}
+            </h2>
+            <PieChart
+              style={{ marginLeft: "vw", height: "200px", width: "200px" }}
+              data={[
+                {
+                  title: "Heard Back: ",
+                  value: allJobs[0].heardBackJobs.length,
+                  color: "#20B2AA",
+                },
+                {
+                  title: "Waiting: ",
+                  value:
+                    allJobs[0].appliedToJobs.length -
+                    allJobs[0].heardBackJobs.length,
+                  color: "#90EE90",
+                },
+              ]}
+              //   label={({ dataEntry }) => dataEntry.title}
+              label={({ dataEntry }) =>
+                `${dataEntry.title} ${Math.round(dataEntry.percentage)} %`
+              }
+            />
+          </div>
+
+          <div id="pie2">
+            <h2 id="heardBack">
+              {" "}
+              Heard Back: {allJobs[0].heardBackJobs.length}{" "}
+            </h2>
+            <PieChart
+              style={{ marginLeft: "vw", height: "200px", width: "200px" }}
+              data={[
+                {
+                  title: "Moved Fwrd: ",
+                  value: allJobs[0].appliedToJobs.length,
+                  color: "#DB7093",
+                },
+                {
+                  title: "Rejected: ",
+                  value: allJobs[0].deniedFromJobs.length,
+                  color: "#ff000080",
+                },
+              ]}
+              //   label={({ dataEntry }) => dataEntry.title}
+              label={({ dataEntry }) =>
+                `${dataEntry.title} ${Math.round(dataEntry.percentage)} %`
+              }
+            />
+          </div>
+        </div>
+        <Link to={"/profile/savedjobs"} className="nav-link">
+          saved Jobs{" "}
+        </Link>
       </div>
     );
   }
