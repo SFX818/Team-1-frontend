427ad31a477800e4d7ca5545b34c173d46d6ee35
kleddy17 <kleddy17@gmail.com>
2021-01-02 20:59:50 -0500

    edit form added, scrollable contacts

--- src/components/Network.js
+++ src/components/Network.js
@@ -2,16 +2,18 @@ import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import authHeader from '../utilities/authHeader.utilities'
 import NetworkForm from './NetworkForm'
+import EditNetworkForm from './EditNetworkForm'
 import {Card, ListGroup, Button} from 'react-bootstrap'
 import {deleteNetwork} from '../services/networkform.service'
 import {getCurrentUser} from '../services/auth.service'
 
 
 
-const Network = (EditNetworkForm) => {
+const Network = () => {
 
 // Setting state to hold the saved networks
 const [networkData, setNetworkData] = useState([])
+const [clicked, setClicked] = useState(false)
     
 // Using the getNetwork function that access our API(backend)
 useEffect (()=>{
@@ -31,17 +33,24 @@ const handleDelete = (network) => {
         deleteNetwork(id);
     }
 
- const handleEdit = (network) => {
-     console.log(network)
-     let id = network._id;
+const  editNetwork = (network ) => {
+     console.log(network);
+    return (
      <EditNetworkForm network = {network}/>
+     )
+ }
+
+ const clickedButton = () => {
+    setClicked(true)
  }
   
-
+console.log(networkData)
 return (
         <div>
-               <NetworkForm/>
-            <div className = "showsavednetwork">
+            
+            <NetworkForm/>
+            <br></br>
+            <div className = "saved-contacts">
               <h1 style = {{textAlign: "center"}}>My Contacts</h1>
                 <ul> 
                     {networkData.map((network) =>(
@@ -55,11 +64,14 @@ return (
                             </ListGroup>
                             <br></br>
                             {/* <Button onClick={() => handleEdit(network)}>Edit</Button> */}
-                            <Button onClick = {()=>handleEdit(network)}>Edit</Button>
+                            {clicked === false ? <Button onClick = {()=>clickedButton()}>Edit</Button> : editNetwork(network) }
+                            {/* <Button onClick = {()=>clickedButton()}>Edit</Button> */}
                             <br></br>
                             <Button onClick= {() => handleDelete(network)}>Delete</Button>
                         </Card>
+                  
                     ))}
+                
                 </ul>
             </div>
         
