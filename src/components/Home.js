d02e7dff8c73cc6f92f90543abc929203f8320f3
kleddy17 <kleddy17@gmail.com>
2021-01-03 18:51:35 -0500

    added dropdown menu functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,12 @@ import axios from "axios";
 import authHeader from "../utilities/authHeader.utilities";
 
 
+
 const Home = () => {
 
+
   const currentUser = getCurrentUser();
+
 
   const [quotes, setQuotes] = useState([]);
 
@@ -14,6 +17,7 @@ const Home = () => {
   useEffect(() => {
     quoteGrabber();
   }, []);
+
 
   const quoteGrabber = () => {
     const quoteData = axios
@@ -32,38 +36,35 @@ const Home = () => {
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
+
   const newQuote = () =>{
     if (quotes.length > 0 ) {
       return <p> "{quotes[0].text}" 
       <br></br>  
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; by <b> {quotes[0].author} </b></p>
+      by <b> {quotes[0].author} </b></p>
     }
   }
 
-
   // create num generator that and put that inside the data array
-
-
-
   
   console.log("THIS IS ALL QUOTES ", quotes[0]);
-
-
-
-
-
-  return <div id="homeBigDiv">
+  return <div>
     
     <div id="home"> 
     <h3 id="userHome">
       <strong> Welcome {currentUser.username} </strong>
     </h3>
-    <br></br>
-    <br></br>
-  
+
    <h5> {newQuote()}</h5>
    </div>
   </div>;
 };
 
 export default Home;
+
+
+
+
+
+
+
